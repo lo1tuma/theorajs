490707a2b59acac41b6a900c9c2550f8f4dddcf4
Mathias Schreck <schreck.mathias@googlemail.com>
2019-04-23 00:25:22 +0200

    Add a more complex video to test suite
    Former-commit-id: b7fa155d2fa71a87836debdec3ee690d9253f00f

---
+++ test/integration/theora/medium.test.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import test from 'ava';
+import { TransportStream } from '../../../src/ogg/transportStream';
+import { ByteStream } from '../../../src/stream/byteStream';
+import { Decoder } from '../../../src/theora/decoder';
+import { readOggFile, decodeAllFrames } from '../../lib/files';
+
+const mediumComplexOggFile = path.resolve(__dirname, '../../fixtures/trailer_400p.ogg');
+
+test('provides the correct meta information about the video stream', async (t) => {
+    const byteStream = await readOggFile(mediumComplexOggFile);
+    const transportStream = new TransportStream(byteStream);
+
+    const [ videoStream ] = transportStream.findLogicalStreams();
+
+    const decoder = new Decoder(videoStream);
+
+    t.is(decoder.width, 720);
+    t.is(decoder.height, 400);
+    t.is(decoder.bitrate, 0);
+    t.deepEqual(decoder.comments, {
+        encoder: 'ffmpeg2theora 0.19'
+    });
+    t.is(decoder.vendor, 'Xiph.Org libTheora I 20060526 3 2 0');
+    t.is(decoder.framerate, 25);
+    t.is(decoder.pixelFormat, 0);
+    t.is(decoder.xOffset, 0);
+    t.is(decoder.yOffset, 0);
+});
+
+test('decodes all frames without errors', async (t) => {
+    const frameCount = await decodeAllFrames(mediumComplexOggFile);
+
+    t.is(frameCount, 813);
+});
