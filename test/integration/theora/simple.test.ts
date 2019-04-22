90b1b9647946a26b85b3a3c57c4b3670bb98cfc7
Mathias Schreck <schreck.mathias@googlemail.com>
2019-04-22 23:27:16 +0200

    Implement simple integration tests for decoding all frames
    Former-commit-id: d409a4b644cf0fc12d02bbbe462d4dfa22bc3b77

---
+++ test/integration/theora/simple.test.ts
@@ -0,0 +1,53 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import test from 'ava';
+import { TransportStream } from '../../../src/ogg/transportStream';
+import { ByteStream } from '../../../src/stream/byteStream';
+import { Decoder } from '../../../src/theora/decoder';
+
+async function readSimpleOggFile(): Promise<ByteStream> {
+    const exampleFile = path.resolve(__dirname, '../../fixtures/320x240.ogg');
+    const data = (await fs.readFile(exampleFile)).toString('binary');
+    const byteStream = new ByteStream();
+
+    byteStream.setData(data);
+
+    return byteStream;
+}
+
+test('provides the correct meta information about the video stream', async (t) => {
+    const byteStream = await readSimpleOggFile();
+    const transportStream = new TransportStream(byteStream);
+
+    const [ videoStream ] = transportStream.findLogicalStreams();
+
+    const decoder = new Decoder(videoStream);
+
+    t.is(decoder.width, 320);
+    t.is(decoder.height, 240);
+    t.is(decoder.bitrate, 0);
+    t.deepEqual(decoder.comments, {});
+    t.is(decoder.vendor, 'Xiph.Org libTheora I 20040317 3 2 0');
+    t.is(decoder.framerate, 30.000299);
+    t.is(decoder.pixelFormat, 0);
+    t.is(decoder.xOffset, 0);
+    t.is(decoder.yOffset, 0);
+});
+
+test('decodes all frames without errors', async (t) => {
+    const byteStream = await readSimpleOggFile();
+    const transportStream = new TransportStream(byteStream);
+
+    const [ videoStream ] = transportStream.findLogicalStreams();
+
+    const decoder = new Decoder(videoStream);
+    const frames = [];
+    let frame = decoder.nextFrame();
+
+    while (frame !== false) {
+        frames.push(frame);
+        frame = decoder.nextFrame();
+    }
+
+    t.is(frames.length, 120);
+});
